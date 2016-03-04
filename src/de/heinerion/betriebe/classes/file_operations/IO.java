9ddef34f1edbc5ef332c8ab22957cfb95588803b
Heinerion <phe0nix@heinerion.de>
2016-03-04 22:28:21 +0100

    massively refactor IO.java

--- src/de/heinerion/betriebe/classes/file_operations/IO.java
+++ src/de/heinerion/betriebe/classes/file_operations/IO.java
@@ -9,7 +9,6 @@ import de.heinerion.betriebe.data.DataBase;
 import de.heinerion.betriebe.data.Session;
 import de.heinerion.betriebe.enums.Pfade;
 import de.heinerion.betriebe.enums.Utilities;
-import de.heinerion.betriebe.exceptions.HeinerionException;
 import de.heinerion.betriebe.loader.TextFileLoader;
 import de.heinerion.betriebe.models.Address;
 import de.heinerion.betriebe.models.Company;
@@ -20,8 +19,12 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static de.heinerion.betriebe.exceptions.HeinerionException.handleException;
 
 public final class IO implements LoadListener {
   private static final Logger logger = LogManager.getLogger(IO.class);
@@ -33,29 +36,22 @@ public final class IO implements LoadListener {
 
   private String lastMessage;
 
-  private static IO me = new IO();
+  static {
+    // Kick off the registrations
+    new IO();
+  }
 
   private IO() {
-    if (logger.isDebugEnabled()) {
-      logger.debug("setup Loaders");
-    }
-    loadingManager.addListener(this);
+    registerListenersAndLoaders();
+  }
 
-    if (logger.isDebugEnabled()) {
-      logger.debug("add CompanyLoader");
-    }
-    final File companyFile = new File(PathTools.getPath(Company.class));
-    final Loader<Company> companyLoader = new CompanyLoader(companyFile);
-    loadingManager.addLoader(Company.class, companyLoader);
+  private void registerListenersAndLoaders() {
+    registerToLoadingManager();
 
-    if (logger.isDebugEnabled()) {
-      logger.debug("add AddressLoader");
-    }
-    final File file = new File(PathTools.getPath(Address.class));
-    final Loader<Address> addressLoader = new AddressLoader(file);
-    loadingManager.addLoader(Address.class, addressLoader);
+    addLoader(Company.class, CompanyLoader.class);
+    addLoader(Address.class, AddressLoader.class);
 
-    // TODO here we don't know even one company
+    // TODO at this point, we don't know even one company
     for (final Company company : Session.getAvailableCompanies()) {
       if (logger.isDebugEnabled()) {
         logger.debug("add invoice loader for {}", company.getDescriptiveName());
@@ -64,7 +60,48 @@ public final class IO implements LoadListener {
     }
   }
 
-  // TODO Vorlagen anders als mit zwei Listen lösen
+  private void registerToLoadingManager() {
+    if (logger.isDebugEnabled()) {
+      logger.debug("setup Loaders");
+    }
+    loadingManager.addListener(this);
+  }
+
+  private <T extends Loadable, X extends Loader<T>> void addLoader(Class<T> classToLoad, Class<X> loaderClass) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("add {}", loaderClass.getSimpleName());
+    }
+
+    File loadDirectory = getLoadDirectory(classToLoad);
+    Loader<T> loader = initLoader(loaderClass, loadDirectory);
+
+    loadingManager.addLoader(classToLoad, loader);
+  }
+
+  private <T extends Loadable> File getLoadDirectory(Class<T> classToLoad) {
+    File loadDirectory = new File(PathTools.getPath(classToLoad));
+    if (logger.isDebugEnabled()) {
+      logger.debug("loadDirectory {}", loadDirectory.getAbsolutePath());
+    }
+    return loadDirectory;
+  }
+
+  private <T extends Loadable, X extends Loader<T>> Loader<T> initLoader(Class<X> loaderClass, File loadDirectory) {
+    Loader<T> loader = null;
+
+    try {
+      Constructor<X> loaderConstructor = loaderClass.getConstructor(File.class);
+      loader = loaderConstructor.newInstance(loadDirectory);
+      if (logger.isDebugEnabled()) {
+        logger.debug("loader {}", loader.getDescriptiveName());
+      }
+    } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+      handleException(getClass(), e);
+    }
+
+    return loader;
+  }
+
   private static String getTemplatePath(Company company) {
     return Pfade.VORLAGEN.file(company.getDescriptiveName());
   }
@@ -74,34 +111,38 @@ public final class IO implements LoadListener {
    * TexVorlagenliste
    */
   public static void ladeSpezielle() {
-    final List<TexVorlage> vorlagen = new ArrayList<>();
-    final File spezial = new File(Pfade.VORLAGENSPEZIAL.getPath());
-    int anzahl = 0;
-    final String[] specials = spezial.list();
+    List<TexVorlage> templates = new ArrayList<>();
+    File special = new File(Pfade.VORLAGENSPEZIAL.getPath());
+    int count = 0;
+    String[] specials = special.list();
     if (null != specials) {
-      for (final String s : specials) {
-        anzahl++;
+      for (String s : specials) {
+        count++;
         if (logger.isDebugEnabled()) {
-          logger.debug("{} spezielle Vorlagen geladen", s);
+          logger.debug("load special template {}", s);
         }
-        vorlagen.add(new TexVorlage(s));
+        templates.add(new TexVorlage(s));
       }
     }
 
     if (logger.isInfoEnabled()) {
-      logger.info(anzahl + " Vorlagen geladen");
+      logger.info(count + " templates loaded");
     }
 
-    for (final TexVorlage template : vorlagen) {
+    for (final TexVorlage template : templates) {
       DataBase.addTexTemplate(null, template);
     }
   }
 
   public static void ladeVorlagen() {
-    for (final Company company : Session.getAvailableCompanies()) {
-      for (final Vorlage template : ladeVorlagen(company)) {
-        DataBase.addTemplate(company, template);
-      }
+    for (Company company : Session.getAvailableCompanies()) {
+      loadAllTemplates(company);
+    }
+  }
+
+  private static void loadAllTemplates(Company company) {
+    for (Vorlage template : ladeVorlagen(company)) {
+      DataBase.addTemplate(company, template);
     }
   }
 
@@ -114,9 +155,9 @@ public final class IO implements LoadListener {
   private static List<Vorlage> ladeVorlagen(Company company) {
     final List<Vorlage> result = FileHandler.load(new Vorlage(),
         getTemplatePath(company));
-    for (Vorlage vorlage : result) {
-      if (vorlage.getInhalt() == null) {
-        vorlage.setInhalt(new String[0][0]);
+    for (Vorlage template : result) {
+      if (template.getInhalt() == null) {
+        template.setInhalt(new String[0][0]);
       }
     }
     return result;
@@ -128,7 +169,7 @@ public final class IO implements LoadListener {
     try {
       loader.saveAddresses(addresses, Utilities.SYSTEM.getPath());
     } catch (final IOException e) {
-      HeinerionException.rethrow(e);
+      handleException(IO.class, e);
     }
   }
 
@@ -178,7 +219,7 @@ public final class IO implements LoadListener {
     IO.ladeSpezielle();
 
     if (progress != null) {
-      progress.setString("fertig...");
+      progress.setString("done...");
     }
   }
 
@@ -189,13 +230,13 @@ public final class IO implements LoadListener {
     }
 
     if (progress != null) {
-      final int maxValue = progress.getMaximum();
-      final int oldValue = progress.getValue();
-      final int newValue = oldValue + 1;
+      int maxValue = progress.getMaximum();
+      int oldValue = progress.getValue();
+      int newValue = oldValue + 1;
       progress.setValue(newValue);
 
-      final double percentage = (double) newValue / maxValue;
-      final String status = message + " ("
+      double percentage = (double) newValue / maxValue;
+      String status = message + " ("
           + FormatTools.formatPercentage(percentage) + ")";
       progress.setString(status);
     }
