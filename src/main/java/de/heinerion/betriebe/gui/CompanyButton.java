36078378d1f29bc80d101f60b9078406329048b7
Heinerion <phe0nix@heinerion.de>
2017-06-11 22:56:17 +0200

    clean up CompanyButton.java

--- src/main/java/de/heinerion/betriebe/gui/CompanyButton.java
+++ src/main/java/de/heinerion/betriebe/gui/CompanyButton.java
@@ -8,28 +8,38 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public final class CompanyButton extends JButton implements CompanyListener {
-  private Company company;
+  private transient Company company;
 
   public CompanyButton(Company aCompany) {
     super(aCompany.getDescriptiveName());
-    this.company = aCompany;
-
-    this.setEnabled(!Session.getActiveCompany().equals(this.company));
-
-    Session.addCompanyListener(this);
-    this.addActionListener(e -> Session.setActiveCompany(this.company));
-  }
-
-  public Company getCompany() {
-    return this.company;
-  }
-
-  @Override
-  public void notifyCompany() {
-    this.setEnabled(!Session.getActiveCompany().equals(this.company));
+    setCompany(aCompany);
+    updateEnabledState();
+    registerListeners();
   }
 
   public void setCompany(Company aCompany) {
     this.company = aCompany;
+  }
+
+  private void updateEnabledState() {
+    setEnabled(!isCurrentlySelected());
+  }
+
+  private boolean isCurrentlySelected() {
+    return Session.getActiveCompany().equals(this.company);
+  }
+
+  private void registerListeners() {
+    Session.addCompanyListener(this);
+    addActionListener(e -> Session.setActiveCompany(this.company));
+  }
+
+  @Override
+  public void notifyCompany() {
+    updateEnabledState();
+  }
+
+  public Company getCompany() {
+    return this.company;
   }
 }
