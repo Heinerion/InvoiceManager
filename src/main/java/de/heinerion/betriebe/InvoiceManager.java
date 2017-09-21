119566c4f1eab0fb150fe78baa791fd6b6bc4a4c
Heinerion <phe0nix@heinerion.de>
2017-09-21 19:57:46 +0200

    make invoiceManager injectable

--- src/main/java/de/heinerion/betriebe/InvoiceManager.java
+++ src/main/java/de/heinerion/betriebe/InvoiceManager.java
@@ -5,13 +5,14 @@ import de.heinerion.aspects.annotations.LogMethod;
 import de.heinerion.betriebe.data.Session;
 import de.heinerion.betriebe.loading.IO;
 import de.heinerion.betriebe.services.ConfigurationService;
+import de.heinerion.betriebe.util.LookAndFeelUtil;
 import de.heinerion.betriebe.view.panels.ApplicationFrame;
 import de.heinerion.betriebe.view.panels.PanelFactory;
 import de.heinerion.betriebe.view.panels.ProgressIndicator;
-import de.heinerion.betriebe.util.LookAndFeelUtil;
 import de.heinerion.exceptions.HeinerionException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.swing.*;
 
@@ -23,17 +24,23 @@ final class InvoiceManager {
 
   private static final long ONE_SECOND = 1000L;
 
-  private InvoiceManager() {
+  private IO io;
+
+  @Autowired
+  private InvoiceManager(IO io) {
+    this.io = io;
   }
 
   @LogMethod
   public static void main(String... args) {
-    parseArguments(args);
-    setup();
-    start();
+    InvoiceManager application = ConfigurationService.getBean(InvoiceManager.class);
+
+    application.parseArguments(args);
+    application.setup();
+    application.start();
   }
 
-  private static void parseArguments(String... args) {
+  private void parseArguments(String... args) {
     Session.isDebugMode(false);
 
     for (String argument : args) {
@@ -45,7 +52,7 @@ final class InvoiceManager {
     }
   }
 
-  private static void evaluateArgument(String string) {
+  private void evaluateArgument(String string) {
     switch (string) {
       case "debug":
         Session.isDebugMode(true);
@@ -54,31 +61,29 @@ final class InvoiceManager {
   }
 
   @LogBefore
-  private static void setup() {
+  private void setup() {
     LookAndFeelUtil.setNimbus();
   }
 
   @LogBefore
-  private static void start() {
+  private void start() {
     prepareApplicationFrame(Session.getFrame());
     startDataThread(Session.getApplicationFrame());
   }
 
-  private static void prepareApplicationFrame(JFrame frame) {
+  private void prepareApplicationFrame(JFrame frame) {
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
   }
 
   @LogBefore
-  private static void startDataThread(ApplicationFrame applicationFrame) {
+  private void startDataThread(ApplicationFrame applicationFrame) {
     new Thread(() -> collectData(applicationFrame)).start();
   }
 
-  private static void collectData(ApplicationFrame applicationFrame) {
+  private void collectData(ApplicationFrame applicationFrame) {
     ProgressIndicator progress = getProgressBarIndicator(applicationFrame);
 
-    // TODO pass to constructor
-    IO io = ConfigurationService.getBean(IO.class);
     // registerListenersAndLoaders needs to be called explicitly here
     // instead of in its constructor to avoid multiple registrations
     io.registerListenersAndLoaders();
@@ -89,11 +94,11 @@ final class InvoiceManager {
     progress.setEnabled(false);
   }
 
-  private static ProgressIndicator getProgressBarIndicator(ApplicationFrame applicationFrame) {
+  private ProgressIndicator getProgressBarIndicator(ApplicationFrame applicationFrame) {
     return PanelFactory.getProgressIndicator(applicationFrame.getProgressBar());
   }
 
-  private static void waitASecond() {
+  private void waitASecond() {
     try {
       Thread.sleep(ONE_SECOND);
     } catch (final InterruptedException e) {
