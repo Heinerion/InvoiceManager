ccda7e73abf225c78466d7663d754cfa207ae82c
Heinerion <phe0nix@heinerion.de>
2017-09-24 15:26:43 +0200

    remove unused constants

--- src/main/java/de/heinerion/util/Constants.java
+++ src/main/java/de/heinerion/util/Constants.java
@@ -3,10 +3,6 @@ package de.heinerion.util;
 public final class Constants {
   public static final int INVOICE_LINE_COUNT = 6;
 
-  public static final String TAB = "\t";
-
-  public static final String SYMBOL_EURO = "€";
-
   private Constants() {
   }
 }
