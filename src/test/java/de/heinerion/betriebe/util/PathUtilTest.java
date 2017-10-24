c345ee58321b9fe3aa61478c8979b67afe921560
Heinerion <phe0nix@heinerion.de>
2017-10-24 19:58:02 +0200

    ban static PathUtil calls from unit tests

--- src/test/java/de/heinerion/betriebe/util/PathUtilTest.java
+++ src/test/java/de/heinerion/betriebe/util/PathUtilTest.java
@@ -21,55 +21,59 @@ public class PathUtilTest {
     return ConfigurationService.get(key);
   }
 
+  private static PathUtilNG pathUtil;
+
   @Before
   public void setUp() throws Exception {
     baseDir = combine(java.lang.System.getProperty("user.home"), property("folder.data"));
+
+    pathUtil = new PathUtilNG();
   }
 
   @Test
   public void getSystemPath() throws Exception {
     String expected = combine(baseDir, property("folder.system"));
-    assertEquals(expected, PathUtil.getSystemPath());
+    assertEquals(expected, pathUtil.getSystemPath());
   }
 
   @Test
   public void getTemplatePath() throws Exception {
     String expected = combine(baseDir, property("folder.system"), property("folder.templates"));
-    assertEquals(expected, PathUtil.getTemplatePath());
+    assertEquals(expected, pathUtil.getTemplatePath());
   }
 
   @Test
   public void getTemplateFolderName() throws Exception {
-    assertEquals(property("folder.templates"), PathUtil.getTemplateFolderName());
+    assertEquals(property("folder.templates"), pathUtil.getTemplateFolderName());
   }
 
   @Test
   public void getTexTemplatePath() throws Exception {
     String expected = combine(baseDir, property("folder.texTemplates"));
-    assertEquals(expected, PathUtil.getTexTemplatePath());
+    assertEquals(expected, pathUtil.getTexTemplatePath());
   }
 
   @Test
   public void determineLetterPath() throws Exception {
     String expected = combine(baseDir, property("folder.letters"));
-    assertEquals(expected, PathUtil.determinePath(Letter.class));
+    assertEquals(expected, pathUtil.determinePath(Letter.class));
   }
 
   @Test
   public void determineInvoicePath() throws Exception {
     String expected = combine(baseDir, property("folder.invoices"));
-    assertEquals(expected, PathUtil.determinePath(Invoice.class));
+    assertEquals(expected, pathUtil.determinePath(Invoice.class));
   }
 
   @Test
   public void getBaseDir() throws Exception {
-    assertEquals(baseDir, PathUtil.getBaseDir());
+    assertEquals(baseDir, pathUtil.getBaseDir());
   }
 
   @Test
   public void getTemplateFileName() throws Exception {
     String descriptiveName = "descriptiveName";
     String expected = combine(baseDir, property("folder.system"), property("folder.templates"), descriptiveName + ".sav");
-    assertEquals(expected, PathUtil.getTemplateFileName(descriptiveName));
+    assertEquals(expected, pathUtil.getTemplateFileName(descriptiveName));
   }
 }
