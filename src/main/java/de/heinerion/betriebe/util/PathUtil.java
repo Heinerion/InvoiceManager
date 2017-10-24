ce5157372d569baeccf5dd65a79c7d65f13c1b6d
Heinerion <phe0nix@heinerion.de>
2017-10-24 21:03:09 +0200

    reduce PathUtil to the minimum required by other classes

--- src/main/java/de/heinerion/betriebe/util/PathUtil.java
+++ src/main/java/de/heinerion/betriebe/util/PathUtil.java
@@ -9,37 +9,17 @@ public class PathUtil {
   private PathUtil() {
   }
 
-  public static String getSystemPath() {
-    return pathUtil.getSystemPath();
-  }
-
-  public static String getTemplatePath() {
-    return pathUtil.getTemplatePath();
-  }
-
-  public static String getTemplateFolderName() {
-    return pathUtil.getTemplateFolderName();
-  }
-
+  /*
+   * still used in TexTemplate constructor
+   */
   public static String getTexTemplatePath() {
     return pathUtil.getTexTemplatePath();
   }
 
+  /*
+   * still used in Company#getFolderFile()
+   */
   public static String determinePath(Class<? extends Letter> itemClass) {
     return pathUtil.determinePath(itemClass);
-  }
-
-  public static String getBaseDir() {
-    return pathUtil.getBaseDir();
-  }
-
-  public static String getTemplateFileName(String descriptiveName) {
-    return pathUtil.getTemplateFileName(descriptiveName);
-  }
-
-  static class NoValidLetterException extends RuntimeException {
-    NoValidLetterException(Class<?> clazz) {
-      super(clazz.getSimpleName() + " is no valid letter extending class");
-    }
   }
 }
