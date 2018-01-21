96e27cc8bc10153467c32671dc4edb6db742312f
Heinerion <phe0nix@heinerion.de>
2018-01-21 20:21:25 +0100

    add deprecations to PathUtil

--- src/main/java/de/heinerion/betriebe/util/PathUtil.java
+++ src/main/java/de/heinerion/betriebe/util/PathUtil.java
@@ -3,22 +3,37 @@ package de.heinerion.betriebe.util;
 import de.heinerion.betriebe.models.Letter;
 import de.heinerion.betriebe.services.ConfigurationService;
 
+/**
+ * Only a wrapper for transition to PathUtilNG
+ *
+ * @deprecated use PathUtilNG instead
+ */
+@Deprecated
 public class PathUtil {
   private static PathUtilNG pathUtil = ConfigurationService.getBean(PathUtilNG.class);
 
   private PathUtil() {
   }
 
+  /**
+   * @deprecated use pathUtil#getTexTemplatePath instead
+   */
   /*
    * still used in TexTemplate constructor
    */
+  @Deprecated
   public static String getTexTemplatePath() {
     return pathUtil.getTexTemplatePath();
   }
 
+
+  /**
+   * @deprecated use PathUtilNG#determinePath instead
+   */
   /*
    * still used in Company#getFolderFile()
    */
+  @Deprecated
   public static String determinePath(Class<? extends Letter> itemClass) {
     return pathUtil.determinePath(itemClass);
   }
