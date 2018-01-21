8626e557d2b19a2257aab39216f4c3a22f0f4c89
Heinerion <phe0nix@heinerion.de>
2018-01-21 20:09:04 +0100

    move LatexPrinter to latex package

--- src/main/java/de/heinerion/betriebe/view/latex/LatexPrinter.java
+++ src/main/java/de/heinerion/betriebe/view/latex/LatexPrinter.java
@@ -1,9 +1,9 @@
-package de.heinerion.betriebe.listener;
+package de.heinerion.betriebe.view.latex;
 
 import de.heinerion.betriebe.boundary.HostSystem;
+import de.heinerion.betriebe.listener.Printer;
 import de.heinerion.betriebe.models.Letter;
 import de.heinerion.betriebe.util.PathUtilNG;
-import de.heinerion.betriebe.view.latex.LatexGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
