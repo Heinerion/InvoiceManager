c3a98600b63a54b3cacaaf6805759e0c20b788d3
Heinerion <phe0nix@heinerion.de>
2018-04-08 09:55:37 +0200

    refactor GlassPane.java to avoid the only once implemented interface

--- src/main/java/de/heinerion/invoice/view/swing/home/GlassPane.java
+++ src/main/java/de/heinerion/invoice/view/swing/home/GlassPane.java
@@ -1,7 +1,49 @@
 package de.heinerion.invoice.view.swing.home;
 
 import javax.swing.*;
+import java.awt.*;
 
-interface GlassPane {
-  JComponent getComponent();
+/**
+ * A Panel which covers the active window, decorates it with a gray overlay and effectively disables it.
+ * <br>
+ * The mouse pointer turns into an hourglass or the systems wait cursor respectively.
+ *
+ * @author heiner
+ */
+class GlassPane {
+  private static final int ALPHA = 150;
+  private final JPanel basePanel;
+
+  /**
+   * Creates a new GlassPane with wait cursor, invisible for now
+   */
+  GlassPane() {
+    basePanel = new JPanel() {
+      @Override
+      protected void paintComponent(Graphics g) {
+        Color bgColor = getBackground();
+
+        g.setColor(getTransparentColor(bgColor));
+        g.fillRect(0, 0, getWidth(), getHeight());
+      }
+    };
+
+    basePanel.setLayout(new BorderLayout());
+    basePanel.setVisible(false);
+    basePanel.setOpaque(false);
+
+    // "hour glass"
+    basePanel.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+  }
+
+  private Color getTransparentColor(Color bgColor) {
+    int red = bgColor.getRed();
+    int blue = bgColor.getBlue();
+    int green = bgColor.getGreen();
+    return new Color(red, blue, green, ALPHA);
+  }
+
+  JComponent getComponent() {
+    return basePanel;
+  }
 }
