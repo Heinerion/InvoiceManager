066536ba7e6b2eb35535466b4528c238d23c21d5
Heinerion <heiner.mueller@stud.fh-luebeck.de>
2018-07-21 15:13:55 +0200

    improve documentation

--- src/main/java/de/heinerion/invoice/Translator.java
+++ src/main/java/de/heinerion/invoice/Translator.java
@@ -1,13 +1,32 @@
 package de.heinerion.invoice;
 
 import java.util.Locale;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
+/**
+ * The Translator reads replacements (or translations) for a given key from a given Resource Bundle
+ */
 public class Translator {
   private Translator() {
   }
 
+  /**
+   * Delivers the value of the given key from a Resource Bundle derived by the key.
+   * <p>
+   * Returns the key enclosed in single quotes if there was no value found for it.
+   * </p>
+   * <p>
+   * The key {@code error.dateFormat} will, for example, be read from the {@code error.properties} Resource Bundle.
+   * </p>
+   *
+   * @param key will be used to determine the Resource Bundle and to retrieve its value from that
+   * @return the value to the given key, if the Resource Bundle could be determined and a value for the given key is defined;<p>
+   * will return the key in single quotes otherwise (e.g. {@code 'unknown.key'})
+   * </p>
+   */
   public static String translate(String key) {
+    Objects.requireNonNull(key);
     return translate(determineResourceBundle(key), key);
   }
 
@@ -38,8 +57,19 @@ public class Translator {
     return "translation." + resourceName;
   }
 
+  /**
+   * Delivers the value of the given key from a Resource Bundle derived by the given class.
+   * <p>
+   * Returns the key enclosed in single quotes if there was no value found for it.
+   * </p>
+   *
+   * @param moduleClass is used to derive the Resource Bundle to be read from
+   * @param key         will be looked for in the Resource Bundle
+   * @return the translation of the given key or, if no translation is found, the key in single quotes
+   */
   public static String translate(Class moduleClass, String key) {
-    return translate(ResourceBundle.getBundle(moduleClass.getCanonicalName()), key);
+    String baseName = Objects.requireNonNull(moduleClass, "moduleClass must not be null").getCanonicalName();
+    return translate(ResourceBundle.getBundle(baseName), Objects.requireNonNull(key, "key must not be null"));
   }
 
   private static String translate(ResourceBundle resourceBundle, String key) {
--- src/main/java/de/heinerion/invoice/print/pdf/boundary/Boundary.java
+++ src/main/java/de/heinerion/invoice/print/pdf/boundary/Boundary.java
@@ -6,6 +6,13 @@ class Boundary {
   private Boundary() {
   }
 
+  /**
+   * delivers the translation of the given key from this packages properties
+   *
+   * @param key the translation key as found in {@code Boundary.properties}
+   * @return the translation of the given key or, if no translation is found, the key in single quotes
+   * @see Translator#translate(Class, String)
+   */
   public static String translate(String key) {
     return Translator.translate(Boundary.class, key);
   }
