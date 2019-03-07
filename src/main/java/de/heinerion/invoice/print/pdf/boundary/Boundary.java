d917b6782e4e98c8d217d89fb1cc9f1c213e83a5
Heinerion <heiner.mueller@stud.fh-luebeck.de>
2019-03-07 20:42:28 +0100

    fix small javadoc issue

--- src/main/java/de/heinerion/invoice/print/pdf/boundary/Boundary.java
+++ src/main/java/de/heinerion/invoice/print/pdf/boundary/Boundary.java
@@ -9,11 +9,14 @@ class Boundary {
   /**
    * delivers the translation of the given key from this packages properties
    *
-   * @param key the translation key as found in {@code Boundary.properties}
+   * @param key       the translation key as found in {@code Boundary.properties}
+   * @param arguments (Optional) Arguments to be substituted in the properties value
+   *
    * @return the translation of the given key or, if no translation is found, the key in single quotes
-   * @see Translator#translate(Class, String)
+   *
+   * @see Translator#translate(Class, String, Object...)
    */
-  public static String translate(String key) {
-    return Translator.translate(Boundary.class, key);
+  public static String translate(String key, Object... arguments) {
+    return Translator.translate(Boundary.class, key, arguments);
   }
 }
