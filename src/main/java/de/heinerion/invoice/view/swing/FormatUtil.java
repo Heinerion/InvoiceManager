b6c0f87039a26c7c302af5bf35d6adf9d5151dc4
Heinerion <heiner.mueller@stud.fh-luebeck.de>
2019-04-02 19:34:26 +0200

    Clean up the FormatUtil
    Get rid of unused DecimalFormats and useless comments

--- src/main/java/de/heinerion/invoice/view/swing/FormatUtil.java
+++ src/main/java/de/heinerion/invoice/view/swing/FormatUtil.java
@@ -5,31 +5,18 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public final class FormatUtil {
-  /* TODO Den ganzen Lokalisierungskäse nochmal überdenken */
-
   private static final String DECIMAL_PATTERN = ",##0.00";
   private static final String PERCENTAGE_PATTERN = "##0%";
 
-  private static DecimalFormat dfAmerican;
   private static DecimalFormat dfLocale;
-  private static DecimalFormat dfPercentage = new DecimalFormat(
-      PERCENTAGE_PATTERN);
+  private static DecimalFormat dfPercentage = new DecimalFormat(PERCENTAGE_PATTERN);
 
   static {
-    final Locale locale = new Locale("en", "US");
-
-    dfAmerican = (DecimalFormat) NumberFormat.getNumberInstance(locale);
-    dfAmerican.applyPattern(DECIMAL_PATTERN);
-
-    dfLocale =   (DecimalFormat) NumberFormat.getNumberInstance(Locale.GERMANY);
+    dfLocale = (DecimalFormat) NumberFormat.getNumberInstance(Locale.GERMANY);
     dfLocale.applyPattern(DECIMAL_PATTERN);
   }
 
   private FormatUtil() {
-  }
-
-  public static String formatAmericanDecimal(double value) {
-    return dfAmerican.format(value);
   }
 
   public static String formatLocaleDecimal(double value) {
