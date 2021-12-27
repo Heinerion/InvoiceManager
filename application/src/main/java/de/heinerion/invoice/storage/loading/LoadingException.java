5c559457230f4c152c2cbb7c167b4f4147f1d312
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2021-12-27 18:44:38 +0100

    delete obsolete loading helper classes

--- application/src/main/java/de/heinerion/invoice/storage/loading/LoadingException.java
+++
@@ -1,7 +0,0 @@
-package de.heinerion.invoice.storage.loading;
-
-class LoadingException extends RuntimeException {
-  <T> LoadingException(Class<T> message) {
-    super("no loader registered for class " + message.getCanonicalName());
-  }
-}
