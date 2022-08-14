907cb4c923213e8f38029519b477c64a741b1d81
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-14 20:47:10 +0200

    create first draft for Entity Objects

---
+++ application/src/main/java/de/heinerion/invoice/models/jpa/AddressEntity.java
@@ -0,0 +1,52 @@
+package de.heinerion.betriebe.models.jpa;
+
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+// TODO embedded
+@NoArgsConstructor
+public class AddressEntity {
+  /**
+   * used as regex to divide address lines
+   */
+  private static final String DELIMITER = "#-#";
+
+  private String addressLine;
+
+  public List<String> getAddressLines() {
+    return addressLine == null
+        ? Collections.emptyList()
+        : Arrays.asList(addressLine.split(DELIMITER));
+  }
+
+  public void setAddressLines(List<String> addressLines) {
+    this.addressLine = String.join(DELIMITER, addressLines);
+  }
+
+  static AddressEntity of(List<String> addressLines) {
+    AddressEntity address = new AddressEntity();
+    address.setAddressLines(addressLines);
+    return address;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AddressEntity that = (AddressEntity) o;
+    return Objects.equals(addressLine, that.addressLine);
+  }
+
+  @Override
+  public int hashCode() {
+    return addressLine.hashCode();
+  }
+}
---
+++ application/src/main/java/de/heinerion/invoice/models/jpa/TemplateEntity.java
@@ -0,0 +1,43 @@
+package de.heinerion.betriebe.models.jpa;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class TemplateEntity {
+  //  @Id
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TemplateEntity that = (TemplateEntity) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    /*
+    fixed value 4 all instances of this entity
+    https://thorben-janssen.com/ultimate-guide-to-implementing-equals-and-hashcode-with-hibernate#Using_a_Generated_Primary_Key
+
+    PRO: contract "hashCode has to be constant while in a hashSet" fulfulled
+    CON: all instances will be assigned the same Hash-Bucket - negative performance
+    OK, BECAUSE: Hibernate is bad with large collections, so the effect ist mitigated
+     */
+    return getClass().getSimpleName().hashCode();
+  }
+}
