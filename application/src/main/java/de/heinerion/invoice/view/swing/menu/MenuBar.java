0e549ef7f628c629b634b5ad46bc7ad142344af3
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-17 19:51:39 +0200

    save companies in invoice numbers menu, if their invoice number has changed

--- application/src/main/java/de/heinerion/invoice/view/swing/menu/MenuBar.java
+++ application/src/main/java/de/heinerion/invoice/view/swing/menu/MenuBar.java
@@ -1,6 +1,7 @@
 package de.heinerion.invoice.view.swing.menu;
 
 import de.heinerion.betriebe.repositories.AddressRepository;
+import de.heinerion.betriebe.repositories.CompanyRepository;
 import de.heinerion.betriebe.repositories.InvoiceRepository;
 import de.heinerion.betriebe.util.PathUtilNG;
 import de.heinerion.invoice.view.swing.menu.info.InfoMenuEntry;
@@ -16,12 +17,14 @@ class MenuBar extends JMenuBar {
   private final transient PathUtilNG pathUtil;
   private final transient AddressRepository addressRepository;
   private final transient InvoiceRepository invoiceRepository;
+  private final transient CompanyRepository companyRepository;
 
-  MenuBar(JFrame origin, PathUtilNG pathUtil, AddressRepository addressRepository, InvoiceRepository invoiceRepository) {
+  MenuBar(JFrame origin, PathUtilNG pathUtil, AddressRepository addressRepository, InvoiceRepository invoiceRepository, CompanyRepository companyRepository) {
     this.origin = origin;
     this.pathUtil = pathUtil;
     this.addressRepository = addressRepository;
     this.invoiceRepository = invoiceRepository;
+    this.companyRepository = companyRepository;
     createWidgets();
     addWidgets();
   }
@@ -30,7 +33,7 @@ class MenuBar extends JMenuBar {
     menuItems = Arrays.asList(
         createItem(new AddressBookMenuEntry(addressRepository)),
         createItem(new InvoicesMenuEntry(invoiceRepository)),
-        createItem(new InvoiceNumbersMenuEntry()),
+        createItem(new InvoiceNumbersMenuEntry(companyRepository)),
         createItem(new InvoiceDateMenuEntry()),
         createItem(new InfoMenuEntry(pathUtil))
     );
