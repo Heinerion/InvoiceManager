4148f51015cae42203b9505cb7a80f5e9fd8e182
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-17 19:53:16 +0200

    save invoice on print

--- application/src/main/java/de/heinerion/invoice/view/swing/home/receiver/PrintButtonPanel.java
+++ application/src/main/java/de/heinerion/invoice/view/swing/home/receiver/PrintButtonPanel.java
@@ -5,26 +5,37 @@ import de.heinerion.betriebe.models.Invoice;
 import de.heinerion.betriebe.repositories.InvoiceRepository;
 import de.heinerion.invoice.print.PrintAction;
 import de.heinerion.invoice.view.swing.home.PanelHolder;
+import lombok.extern.flogger.Flogger;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
+@Flogger
 @Service
 public class PrintButtonPanel implements PanelHolder {
-  private SidePanel sidePanel;
+  private final SidePanel sidePanel;
+  private final InvoiceRepository invoiceRepository;
 
   PrintButtonPanel(PrintAction printAction, InvoiceRepository invoiceRepository) {
-    sidePanel = new SidePanel();
+    this.invoiceRepository = invoiceRepository;
+    this.sidePanel = new SidePanel();
+    sidePanel.add(createPrintButton(printAction));
+  }
 
-    final JButton btnDrucken = new JButton("Drucken");
+  private JButton createPrintButton(PrintAction printAction) {
+    JButton btnDrucken = new JButton("Drucken");
     btnDrucken.setName("print");
-    sidePanel.add(btnDrucken);
     btnDrucken.addActionListener(printAction);
-    btnDrucken.addActionListener(e -> {
-      if (Session.getActiveConveyable() instanceof Invoice invoice) {
-        invoiceRepository.save(invoice);
-      }
-    });
+    btnDrucken.addActionListener(this::saveInvoice);
+    return btnDrucken;
+  }
+
+  private void saveInvoice(ActionEvent ignored) {
+    if (Session.getActiveConveyable() instanceof Invoice invoice) {
+      log.atInfo().log("save invoice %s", invoice);
+      invoiceRepository.save(invoice);
+    }
   }
 
   @Override
