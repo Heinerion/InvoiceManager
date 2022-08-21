dbe20d37198d17c6fd629ebf4e04c5611ee480b1
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-21 02:51:53 +0200

    add persistence for letters

--- application/src/main/java/de/heinerion/invoice/view/swing/home/receiver/PrintButtonPanel.java
+++ application/src/main/java/de/heinerion/invoice/view/swing/home/receiver/PrintButtonPanel.java
@@ -2,7 +2,9 @@ package de.heinerion.invoice.view.swing.home.receiver;
 
 import de.heinerion.betriebe.data.Session;
 import de.heinerion.betriebe.models.Invoice;
+import de.heinerion.betriebe.models.Letter;
 import de.heinerion.betriebe.repositories.InvoiceRepository;
+import de.heinerion.betriebe.repositories.LetterRepository;
 import de.heinerion.invoice.print.PrintAction;
 import de.heinerion.invoice.view.swing.home.PanelHolder;
 import lombok.extern.flogger.Flogger;
@@ -16,9 +18,11 @@ import java.awt.event.ActionEvent;
 public class PrintButtonPanel implements PanelHolder {
   private final SidePanel sidePanel;
   private final InvoiceRepository invoiceRepository;
+  private final LetterRepository letterRepository;
 
-  PrintButtonPanel(PrintAction printAction, InvoiceRepository invoiceRepository) {
+  PrintButtonPanel(PrintAction printAction, InvoiceRepository invoiceRepository, LetterRepository letterRepository) {
     this.invoiceRepository = invoiceRepository;
+    this.letterRepository = letterRepository;
     this.sidePanel = new SidePanel();
     sidePanel.add(createPrintButton(printAction));
   }
@@ -32,9 +36,13 @@ public class PrintButtonPanel implements PanelHolder {
   }
 
   private void saveInvoice(ActionEvent ignored) {
+    Letter letter = Session.getActiveConveyable();
     if (Session.getActiveConveyable() instanceof Invoice invoice) {
       log.atInfo().log("save invoice %s", invoice);
       invoiceRepository.save(invoice);
+    } else {
+      log.atInfo().log("save letter %s", letter);
+      letterRepository.save(letter);
     }
   }
 
