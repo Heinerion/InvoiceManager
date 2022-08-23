85a31f35920765cc3ea9da83dc9a298ebe3321a5
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-23 20:48:16 +0200

    upgrade most dependencies (jaxb is left)

--- application/src/test/java/de/heinerion/invoice/view/swing/ApplicationFrameIT.java
+++ application/src/test/java/de/heinerion/invoice/view/swing/ApplicationFrameIT.java
@@ -17,7 +17,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.swing.*;
 import java.time.Month;
