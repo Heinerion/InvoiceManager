762fa863e697ec669c917893dfafa2919e08ccfd
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-24 20:28:49 +0200

    delete unused files

--- application/src/main/java/de/heinerion/invoice/boundary/WriterFactory.java
+++
@@ -1,9 +0,0 @@
-package de.heinerion.invoice.boundary;
-
-import java.io.*;
-
-public class WriterFactory {
-  public Writer create(File file) throws IOException {
-    return new BufferedWriter(new FileWriter(file));
-  }
-}
