9ea720d1d2058ba848b072ebe32239c8a3e60504
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2022-08-30 22:35:49 +0200

    refactor PrintOperations

--- application/src/main/java/de/heinerion/invoice/print/PrintOperations.java
+++ application/src/main/java/de/heinerion/invoice/print/PrintOperations.java
@@ -2,30 +2,19 @@ package de.heinerion.invoice.print;
 
 import de.heinerion.invoice.models.Conveyable;
 import de.heinerion.invoice.models.Invoice;
-import de.heinerion.invoice.models.Letter;
 import de.heinerion.invoice.util.PathUtilNG;
 import de.heinerion.invoice.view.DateUtil;
 import lombok.extern.flogger.Flogger;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Flogger
 @Service
 public class PrintOperations {
-  private static final int STATE_LETTER = 0;
-  private static final int STATE_INVOICE = 1;
-
   private final Printer printer;
   private final PathUtilNG pathUtil;
-
-  private final FileInfoGenerator[] generators = {
-      new LetterInfoGenerator(),
-      new InvoiceInfoGenerator()};
-  private int state = STATE_LETTER;
 
   PrintOperations(Printer printer, PathUtilNG pathUtil) {
     this.printer = printer;
@@ -46,44 +35,24 @@ public class PrintOperations {
    *     the conveyable to be written to pdf
    */
   public void createDocument(Conveyable letter) {
-    updateState(letter);
+    SwingUtilities.invokeLater(() -> createDocumentLater(letter));
+  }
 
+  protected void createDocumentLater(Conveyable letter) {
     String title = generateTitle(letter);
     Path folder = generatePath(letter);
     log.atInfo().log("print %s to %s", title, folder);
-    SwingUtilities.invokeLater(() -> printer.writeFile(letter, folder, title));
-  }
-
-  private void updateState(Conveyable letter) {
-    if (letter instanceof Invoice) {
-      state = STATE_INVOICE;
-    } else {
-      state = STATE_LETTER;
-    }
+    printer.writeFile(letter, folder, title);
   }
 
   private Path generatePath(Conveyable conveyable) {
-    Path folder = getGenerator().getFolder(conveyable);
-
-    if (Files.exists(folder)) {
-      return folder;
-    }
-
-    log.atInfo().log("create directory %s", folder);
-    try {
-      return Files.createDirectories(folder);
-    } catch (IOException e) {
-      throw new RuntimeException(
-          String.format("directory %s could not be created", folder), e);
-    }
-  }
-
-  private FileInfoGenerator getGenerator() {
-    return generators[state];
+    return (conveyable instanceof Invoice)
+        ? pathUtil.determineInvoicePath(conveyable.getCompany())
+        : pathUtil.determineLetterPath();
   }
 
   private String generateTitle(Conveyable letter) {
-    String start = getGenerator().getTitle(letter);
+    String start = getTitle(letter);
 
     String receiver = letter.getReceiver().getRecipient();
     String date = DateUtil.format(letter.getDate());
@@ -91,33 +60,9 @@ public class PrintOperations {
     return String.join(" ", start, receiver, date);
   }
 
-  private interface FileInfoGenerator {
-    String getTitle(Conveyable conveyable);
-
-    Path getFolder(Conveyable conveyable);
-  }
-
-  private class LetterInfoGenerator implements FileInfoGenerator {
-    @Override
-    public String getTitle(Conveyable letter) {
-      return ((Letter) letter).getSubject() + " --";
-    }
-
-    @Override
-    public Path getFolder(Conveyable conveyable) {
-      return pathUtil.determineLetterPath();
-    }
-  }
-
-  private class InvoiceInfoGenerator implements FileInfoGenerator {
-    @Override
-    public String getTitle(Conveyable invoice) {
-      return Integer.toString(((Invoice) invoice).getNumber());
-    }
-
-    @Override
-    public Path getFolder(Conveyable invoice) {
-      return pathUtil.determineInvoicePath(invoice.getCompany());
-    }
+  public String getTitle(Conveyable conveyable) {
+    return (conveyable instanceof Invoice invoice)
+        ? Integer.toString((invoice).getNumber())
+        : conveyable.getSubject() + " --";
   }
 }
---
+++ application/src/test/java/de/heinerion/invoice/print/PrintOperationsTest.java
@@ -0,0 +1,81 @@
+package de.heinerion.invoice.print;
+
+import de.heinerion.invoice.models.*;
+import de.heinerion.invoice.util.PathUtilNG;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrintOperationsTest {
+
+  @Test
+  public void createDocumentLater_letter() {
+    TestPrinter printer = new TestPrinter();
+    PathUtilNG pathUtil = mockPathUtil();
+
+    Letter letter = new Letter()
+        .setSubject("Subject")
+        .setReceiver(new Address().setRecipient("Recipient"))
+        .setDate(LocalDate.of(2022, 8, 30));
+
+    new PrintOperations(printer, pathUtil).createDocumentLater(letter);
+
+    assertEquals(letter, printer.conveyable);
+    assertEquals("Subject -- Recipient 30.08.2022", printer.title);
+    assertEquals("letterDir", printer.parentFolder.toString());
+  }
+
+  @Test
+  public void createDocumentLater_invoice() {
+    TestPrinter printer = new TestPrinter();
+    PathUtilNG pathUtil = mockPathUtil();
+
+    Invoice invoice = new Invoice()
+        .setNumber(1)
+        .setCompany(new Company().setDescriptiveName("descriptiveName"))
+        .setReceiver(new Address().setRecipient("Recipient"))
+        .setDate(LocalDate.of(2022, 8, 30));
+
+    new PrintOperations(printer, pathUtil).createDocumentLater(invoice);
+
+    assertEquals(invoice, printer.conveyable);
+    assertEquals("1 Recipient 30.08.2022", printer.title);
+    assertEquals("invoiceDir" + File.separator + "descriptiveName",
+        printer.parentFolder.toString());
+  }
+
+  private PathUtilNG mockPathUtil() {
+    PathUtilNG pathUtil = EasyMock.niceMock(PathUtilNG.class);
+    EasyMock
+        .expect(pathUtil.determineLetterPath())
+        .andReturn(Path.of("letterDir"))
+        .anyTimes();
+    EasyMock
+        .expect(pathUtil.determineInvoicePath(EasyMock.anyObject(Company.class)))
+        .andAnswer(() -> Path
+            .of("invoiceDir")
+            .resolve(((Company) EasyMock.getCurrentArguments()[0]).getDescriptiveName()))
+        .anyTimes();
+    EasyMock.replay(pathUtil);
+    return pathUtil;
+  }
+
+  private final class TestPrinter implements Printer {
+    private Conveyable conveyable;
+    private Path parentFolder;
+    private String title;
+
+    @Override
+    public void writeFile(Conveyable conveyable, Path parentFolder, String title) {
+      System.out.println(conveyable + "::" + title + " @ " + parentFolder);
+      this.conveyable = conveyable;
+      this.parentFolder = parentFolder;
+      this.title = title;
+    }
+  }
+}
