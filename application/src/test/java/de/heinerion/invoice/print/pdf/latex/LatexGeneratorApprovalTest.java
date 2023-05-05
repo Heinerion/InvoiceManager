f539859d4f1a38e92f9bcd96f08a0a3649268e9a
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-05-05 22:58:20 +0200

    replace string-based approvals with real (file-based) approval tests

---
+++ application/src/test/java/de/heinerion/invoice/print/pdf/latex/LatexGeneratorApprovalTest.java
@@ -0,0 +1,88 @@
+package de.heinerion.invoice.print.pdf.latex;
+
+import de.heinerion.invoice.models.*;
+import de.heinerion.invoice.testsupport.builder.*;
+import de.heinerion.invoice.view.formatter.FormatterMock;
+import org.approvaltests.Approvals;
+import org.junit.*;
+
+import java.time.*;
+
+public class LatexGeneratorApprovalTest {
+  private static final Month MONTH = Month.JUNE;
+  private static final int YEAR = 2010;
+  private static final int DAY = 25;
+
+  private static Company sender;
+  private static Address receiverAddress;
+  private static LocalDate date;
+
+  private static Letter letter;
+  private static Invoice invoice;
+
+  @BeforeClass
+  public static void prepare() {
+    sender = new CompanyBuilder().build();
+    receiverAddress = new AddressBuilder()
+        .withDistrict("")
+        .withApartment("")
+        .build();
+    date = LocalDate.of(YEAR, MONTH, DAY);
+  }
+
+  @Before
+  public final void setUp() {
+    letter = new Letter(date, sender, receiverAddress);
+    letter.setSubject("Test");
+
+    invoice = new Invoice(date, sender, receiverAddress, sender.getInvoiceNumber());
+    invoice.addItem(Item.of(invoice.getItems().size(), "Artikel 1", "Stück", 1.50, 2));
+  }
+
+  protected LatexGenerator getLatexGenerator() {
+    return new LatexGeneratorFreeMarkerImpl(new FormatterMock());
+  }
+
+  @Test
+  public final void generateEmptyLetter() {
+    final String result = getLatexGenerator().generateSourceContent(letter);
+
+    Approvals.verify(result);
+  }
+
+  @Test
+  public final void generateNonEmptyLetter() {
+    String content = "Hello new World";
+    letter.addMessageLine(content);
+    letter.addMessageLine(content);
+    letter.addMessageLine(content);
+    letter.addMessageLine(content);
+    String result = getLatexGenerator().generateSourceContent(letter);
+
+    Approvals.verify(result);
+  }
+
+  @Test
+  public final void generateInvoice() {
+    final String result = getLatexGenerator().generateSourceContent(invoice);
+
+    Approvals.verify(result);
+  }
+
+  @Test
+  public final void generateInvoice_items() {
+    invoice.addItem(Item.of(invoice.getItems().size(), "Artikel 2", "Stück", 3.44, 3));
+    final String result = getLatexGenerator().generateSourceContent(invoice);
+
+    Approvals.verify(result);
+  }
+
+  @Test
+  public final void generateInvoice_text() {
+    invoice.addItem(Item.of(invoice.getItems().size(), "Message"));
+
+    final String result = getLatexGenerator().generateSourceContent(invoice);
+
+    Approvals.verify(result);
+  }
+}
--- application/src/test/java/de/heinerion/invoice/view/formatter/FormatterMockTest.java
+++ application/src/test/java/de/heinerion/invoice/view/formatter/FormatterMockTest.java
@@ -2,28 +2,18 @@ package de.heinerion.invoice.view.formatter;
 
 import de.heinerion.invoice.testsupport.builder.AddressBuilder;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
 public class FormatterMockTest {
-  @Autowired
-  private Formatter formatter;
-
   @Test
   public void formatAddress() {
     List<String> expected = new ArrayList<>();
     expected.add("formatted");
     expected.add("address");
 
-    assertEquals(expected, formatter.formatAddress(new AddressBuilder().build()));
+    assertEquals(expected, ((Formatter) new FormatterMock()).formatAddress(new AddressBuilder().build()));
   }
 }
