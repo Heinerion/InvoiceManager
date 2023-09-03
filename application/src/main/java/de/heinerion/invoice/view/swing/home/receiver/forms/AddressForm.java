50a655fab279fa49d52c4acb82e9b33497b85109
Heiner Specht <94306779+HeinerSpecht@users.noreply.github.com>
2023-09-03 21:53:03 +0200

    Update address form

--- application/src/main/java/de/heinerion/invoice/view/swing/home/receiver/forms/AddressForm.java
+++ application/src/main/java/de/heinerion/invoice/view/swing/home/receiver/forms/AddressForm.java
@@ -1,14 +1,15 @@
 package de.heinerion.invoice.view.swing.home.receiver.forms;
 
 import de.heinerion.invoice.models.Address;
+import de.heinerion.util.Strings;
 
 import java.util.*;
 
 public class AddressForm extends AbstractForm<Address> {
 
   private final List<FormLine<Address, ?>> properties = Arrays.asList(
-      FormLine.of(Address.class, String.class).name("name").setter(Address::setName).build(),
-      FormLine.of(Address.class, String.class).name("block").setter(Address::setBlock).build()
+      FormLine.of(Address.class, String.class).name("name").setter(Address::setName).valid(Strings::isNotBlank).build(),
+      FormLine.of(Address.class, String.class).name("block").setter(Address::setBlock).valid(Strings::isNotBlank).build()
   );
 
   @Override
