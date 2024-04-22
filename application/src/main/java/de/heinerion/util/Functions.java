d69340ed188b3d7486140d03bcfa5547e40b5026
Heinerion <13521176+Heinerion@users.noreply.github.com>
2024-04-22 10:31:54 +0200

    Add documentation to Functions

--- application/src/main/java/de/heinerion/util/Functions.java
+++ application/src/main/java/de/heinerion/util/Functions.java
@@ -1,13 +1,18 @@
 package de.heinerion.util;
 
+/** Collection of convenience Methods instead of repeated Lambdas */
 public class Functions {
-  public static <A, B> void doNothing(A a, B b) {
+
+  /** {@link java.util.function.BiConsumer} doing nothing */
+  public static <A, B> void doNothing(A ignoredA, B ignoredB) {
   }
 
+  /** @return true in every case */
   public static <T> boolean alwaysTrue(T ignored) {
     return true;
   }
 
+  /** @return false in every case */
   public static <T> boolean alwaysFalse(T ignored) {
     return false;
   }
