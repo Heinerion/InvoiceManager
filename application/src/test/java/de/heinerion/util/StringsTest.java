57a90510150fee420bf73df35bc859c673d48c94
Heinerion <13521176+Heinerion@users.noreply.github.com>
2024-04-26 22:02:01 +0200

    Strengthen unit tests for Strings
    Lead by mutation testing

--- application/src/test/java/de/heinerion/util/StringsTest.java
+++ application/src/test/java/de/heinerion/util/StringsTest.java
@@ -2,34 +2,78 @@ package de.heinerion.util;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static de.heinerion.util.Strings.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StringsTest {
   @Test
-  void isBlank_null() {
-    assertTrue(Strings.isBlank(null));
+  @DisplayName("isBlank: Should accept null")
+  public void isBlankShouldAcceptNull() {
+    assertTrue(isBlank(null));
   }
 
   @Test
-  void isBlank_empty() {
-    assertTrue(Strings.isBlank(""));
+  @DisplayName("isBlank: Should accept the empty String")
+  public void isBlankShouldAcceptTheEmptyString() {
+    assertTrue(isBlank(""));
   }
 
   @Test
-  void isBlank_singleWhitespace() {
-    assertTrue(Strings.isBlank(" "));
+  @DisplayName("isBlank: Should accept whitespaces")
+  public void isBlankShouldAcceptWhitespaces() {
+    assertTrue(isBlank(" "));
+    assertTrue(isBlank(" \t"));
   }
 
   @Test
-  void isBlank_multipleWhitespaces() {
-    assertTrue(Strings.isBlank(" \t"));
+  @DisplayName("isBlank: Should accept line endings")
+  public void isBlankShouldAcceptLineEndings() {
+    assertTrue(isBlank("\n"));
+    assertTrue(isBlank("\r"));
+    assertTrue(isBlank("\r\n"));
   }
 
   @Test
-  void isBlank_lineBreaks() {
-    assertTrue(Strings.isBlank("\n"));
-    assertTrue(Strings.isBlank("\r"));
-    assertTrue(Strings.isBlank("\r\n"));
+  @DisplayName("isBlank: Should reject any alphanumerics")
+  public void isBlankShouldRejectAnyAlphanumerics() {
+    assertFalse(isBlank("0"));
+    assertFalse(isBlank("42"));
+    assertFalse(isBlank("foo"));
+  }
+
+  @Test
+  @DisplayName("isNotBlank: Should reject null")
+  public void isNotBlankShouldRejectNull() {
+    assertFalse(isNotBlank(null));
+  }
+
+  @Test
+  @DisplayName("isNotBlank: Should reject the empty String")
+  public void isNotBlankShouldRejectTheEmptyString() {
+    assertFalse(isNotBlank(""));
+  }
+
+  @Test
+  @DisplayName("isNotBlank: Should reject whitespaces")
+  public void isNotBlankShouldRejectWhitespaces() {
+    assertFalse(isNotBlank(" "));
+    assertFalse(isNotBlank(" \t"));
+  }
+
+  @Test
+  @DisplayName("isNotBlank: Should reject line endings")
+  public void isNotBlankShouldRejectLineEndings() {
+    assertFalse(isNotBlank("\n"));
+    assertFalse(isNotBlank("\r"));
+    assertFalse(isNotBlank("\r\n"));
+  }
+
+  @Test
+  @DisplayName("isNotBlank: should accept alphanumerics")
+  public void isNotBlankShouldAcceptAlphanumerics() {
+    assertTrue(isNotBlank("0"));
+    assertTrue(isNotBlank("42"));
+    assertTrue(isNotBlank("foo"));
   }
 }
